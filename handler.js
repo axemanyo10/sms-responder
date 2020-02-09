7177ac74331ac3d7f688de133b9d7426acd3826f
Fergus McGlynn <theferg@gmail.com>
2020-02-09 19:07:33 +0000

    Incorporate chat functionality

--- handler.js
+++ handler.js
@@ -1,16 +1,32 @@
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const querystring = require('querystring');
+const fetch = require("node-fetch");
 
-const generateResponse = (input) => {
+const url = "https://some-random-api.ml/chatbot?message=";
+
+const getData = async (url, query) => {
+  try {
+    const response = await fetch(url + query);
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const generateResponse = async (input) => {
   const twiml = new MessagingResponse();
-  twiml.message('Hello from the ' + process.env.DEPLOY_ENV + ' environment! You said "' + input.Body + '"');
+  const chatResponse = await getData(url, input.Body);
+  twiml.message(chatResponse.response);
   return twiml.toString();
 };
 
-module.exports.hello = async event => {
+module.exports.chat = async event => {
+  const response = await generateResponse(querystring.parse(event.body))
   return {
     statusCode: 200,
     headers: {'Content-Type': 'text/xml'},
-    body: generateResponse(querystring.parse(event.body)),
+    body: response,
   };
 };
